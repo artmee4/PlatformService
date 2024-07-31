3dc5f9e3775e8a600093cb7288aa98437e420119
Django <gorshkov.artem2010@yandex.ru>
2024-07-31 09:17:57 +0300

    first

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app, bool isProd) 
+        { 
+            using(var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>(), isProd);
+            }
+        }
+
+        private static void SeedData(AppDbContext context, bool isProd)
+        {
+            if (isProd)
+            {
+                Console.WriteLine("--> Attempting to aply migration...");
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception ex) { Console.WriteLine($"Could not run Migration: {ex.Message}"); }
+            }
+
+            if(!context.Platforms.Any()) 
+            {
+                Console.WriteLine("--> Seeding data ...");
+
+                context.Platforms.AddRange(
+                    new Models.Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost="Free"},
+                    new Models.Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Models.Platform() { Name = "Kubernetes", Publisher = "CLoud Native Computing Foundation", Cost = "Free" }
+                );
+                
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
+        }
+    }
+}
---
+++ PlatformService/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace PlatformService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+}
+// docker build -t commandservice .
+// docker push commandservice
+// docker run -p 127.0.0.1:5156:80 -d platformservice
+// C:\Windows\System32\drivers\etc\hosts - добавить acme.com
